30bf349eefa0ec1d27763930bf8cfa99f1a7f200
TanCodes <barvitanmay20@gmail.com>
2022-06-24 01:10:10 +0530

    Updated code

--- script/script.js
+++ script/script.js
@@ -19,32 +19,6 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
-// animation text (Mobile view)
-  const text1 = ['Increase Concentration','Daily Reading Habit',"Boost Memory Retention","With Refined Experience"];
-  let count1 = 0;
-  let index1 = 0;
-  
-  let letter1 = [''];
-  
-  (function typeanimate(){
-  
-    if (count1 === text1.length){
-      count1 = 0;
-    }
-  
-    currentTex1 = text1[count1];
-    letter1 = currentTex1.slice(0, ++index1);
-  
-    document.querySelector('.text-animation1').textContent=letter1;
-  
-    if(letter1.length === currentTex1.length){
-      count1++;
-      index1=0;
-    }
-  
-    setTimeout(typeanimate, 300);
-  }());
-
 // Fade animation -  AOS 
 AOS.init({
     offset: 300,
